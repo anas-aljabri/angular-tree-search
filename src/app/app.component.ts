0c4bb92b298bc10fa3d3c202cbd4e5460f6c80dd
Anas Aljabri <32190974+anas-aljabri@users.noreply.github.com>
2019-01-06 22:10:30 +0000

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'angular-tree-search';
-  tree: Node[]; 
+  tree: Node[];
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
-    this.dataService.getTree('./assets/data.json').subscribe(result =>
-      this.tree = result);
+    this.dataService.getTree('./assets/data.json').subscribe(result => {
+      this.tree = result;
+
+      this.addSalesFieldToLowestLevelChildren(this.tree);
+
+      this.calcAccumulatedTotals(this.tree);
+
+      console.log(this.tree);
+
+      this.saveFile('data - sales.json', JSON.stringify(this.tree));
+    });
+  }
+
+  addSalesFieldToLowestLevelChildren(tree: Node[]) {
+    RecursiveLoop(tree);
+
+    function RecursiveLoop(currentTree: Node[]) {
+      currentTree.forEach(node => {
+        if (node.Children) {
+          RecursiveLoop(node.Children);
+        }
+        else
+          node['Fields'] = [ Math.round(Math.random() * 100000)] 
+      })
+    }
+  }
+
+  calcAccumulatedTotals(tree: Node[]) {
+    tree.forEach(node => {
+      if (node.Children) {
+
+        //  To check that all children have the sales field
+        let allChildrenHaveSales = true;
+
+        node.Children.forEach(child => {
+          if (!child['Fields']) {
+            allChildrenHaveSales = false;
+          }
+        });
+
+        if (!allChildrenHaveSales) {
+          this.calcAccumulatedTotals(node.Children);
+        }
+
+        let salesValue = node.Children.map(child => {
+          return child['Fields'][0]
+        }).reduce((current, acc) => {
+          return current + acc;
+        });
+
+        node['Fields'] = [salesValue] 
+      }
+    }
+    )
+  }
+
+  saveFile(fileName: string, fileBody: string) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/json;charset=utf-8,%EF%BB%BF' + encodeURIComponent(fileBody));
+    element.setAttribute('download', fileName);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
   }
 }
